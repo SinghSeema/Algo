a7b433c16a6522286fb8d861a70c4f14c85d60b1
SinghSeema <seema.tomar85@gmail.com>
2018-07-29 18:39:49 +0530

    Delete a node from binary search tree

---
+++ DeleteNode.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+    int data;
+    Node* left;
+    Node* right;
+    public:
+    Node(int data) {
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+    void insert(int data);
+    bool find(int data);
+    Node* delete_node(int data);
+    void inorder();
+    
+};
+void Node::insert(int data) {
+    if(data < this->data) {
+        if ( left != NULL)
+            left->insert(data);
+        else
+            left = new Node(data);
+        
+    }
+    else if (data >= this->data) {
+        if(right != NULL)
+            right->insert(data);
+        else
+            right = new Node(data);
+        
+    }
+}
+void Node::inorder() {
+    if(left != NULL)
+        left->inorder();
+    cout<<data<<" ";
+    if(right != NULL)
+        right->inorder();
+        
+}
+
+bool Node::find(int data) {
+    if (data == this->data)
+        return true;
+    else if (data < this->data) {
+        if (left == NULL)
+            return false;
+        else
+        left->find(data);
+        
+    }
+    else if (data > this->data) {
+        if(right == NULL)
+            return false;
+        else 
+        right->find(data);
+        
+    }
+
+}
+
+Node* Node::delete_node(int data) {
+    if (data < this->data) {
+        if (left != NULL) {
+            left = left->delete_node(data);
+        }
+            
+    }
+    else if (data > this->data) {
+        if (right != NULL) {
+            right = right->delete_node(data);
+        }
+    }
+    else {
+        if (left == NULL && right == NULL) {
+            delete this;
+            return NULL;
+        }
+        else if (left != NULL) {
+            Node * tmp = this->left;
+            delete this;
+            return tmp;
+        }
+        else if (right != NULL) {
+            Node* tmp = this->right;
+            delete this;
+            return tmp;
+        }
+        
+    }
+
+}
+
+
+int main() {
+	cout<<"create Tree!"<<endl;
+	Node * Tree = new Node(6);
+	Tree->insert(3);
+	Tree->insert(1);
+	Tree->insert(8);
+	Tree->insert(7);
+	Tree->inorder();
+	Tree->delete_node(8);
+	cout<<endl<<"Tree Traversal after delete"<<endl;
+	Tree->inorder();
+	
+	return 0;
+}
